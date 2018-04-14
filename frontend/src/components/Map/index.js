ed46a8423c06ff2c1697463624b3fd0ca7e094b8
Lucas Sacramento <lucas.arki@gmail.com>
2018-04-14 10:48:44 +0100

    Implemented React google maps

---
+++ frontend/src/components/Map/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { compose, withProps } from "recompose"
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
+import './styles.css';
+
+const Map = compose(
+  withProps({
+    googleMapURL: "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places",
+    loadingElement: <div style={{ height: `100%` }} />,
+    containerElement: <div style={{ height: `400px` }} />,
+    mapElement: <div style={{ height: `100%` }} />,
+  }),
+  withScriptjs,
+  withGoogleMap
+)((props) =>
+  <GoogleMap
+    defaultZoom={8}
+    defaultCenter={{ lat: -34.397, lng: 150.644 }}
+  >
+    {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} />}
+  </GoogleMap>
+);
+
+export default Map;

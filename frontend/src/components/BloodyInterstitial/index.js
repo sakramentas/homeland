c5631d37bec33de39b35d43b1299e58830b354c5
Lucas Sacramento <lucas.arki@gmail.com>
2018-04-15 12:22:30 +0100

    Finished interstitial

--- frontend/src/components/BloodyInterstitial/index.js
+++ frontend/src/components/BloodyInterstitial/index.js
@@ -3,7 +3,10 @@ import React from 'react';
 import './interstitial.css';
 
 const Interstitial = ({ show }) => (
-  <div className={`interstitialWrapper ${show ? 'show' : ''}`}>Icon</div>
+  <div className={`interstitialWrapper ${show ? 'show' : ''}`}>
+    <img src={require('../../assets/interstitial.gif')} alt="interstitial"/>
+    <span>Finding the best properties based on your data</span>
+  </div>
 );
 
 export default Interstitial;

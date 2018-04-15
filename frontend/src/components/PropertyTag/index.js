b9723567e68d134ca01bf90d39b95b7ae2425fbb
Lucas Sacramento <lucas.arki@gmail.com>
2018-04-15 11:58:08 +0100

    adding logos

--- frontend/src/components/PropertyTag/index.js
+++ frontend/src/components/PropertyTag/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './styles.css';
 
-const PropertyTag = (props) => {
+const PropertyTag = ({ affordabilityStatus }) => {
+  const affordabilityMessage = {
+    green: 'The property matches your profile',
+    amber: 'The property is close to match your profile',
+    red: 'The property does not match your profile',
+  }[affordabilityStatus];
+
   return (
-    <div className="property-tag">
-      <span>The property matches your profile</span>
+    <div className={`property-tag ${affordabilityStatus}`}>
+      <span>{affordabilityMessage}</span>
     </div>
   );
 };
